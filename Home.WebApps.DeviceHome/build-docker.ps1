93acdf8816932cf7957bd7304c35a003746082a8
Michael CARBENAY <michael@carbenay.info>
2022-10-29 14:08:06 +0200

    Suppr de l'app device home (migré dans le homegraph)

--- Home.WebApps.DeviceHome/build-docker.ps1
+++
@@ -1,9 +0,0 @@
-﻿docker build . -f ..\..\..\Dockerfile.Release -t 192.168.2.184:5000/webapps-devicehome:latest
-
-docker tag 192.168.2.184:5000/webapps-devicehome:latest 192.168.2.100:5000/webapps-devicehome:latest
-
-docker push 192.168.2.184:5000/webapps-devicehome:latest
-docker push 192.168.2.100:5000/webapps-devicehome:latest
-
-docker image rm 192.168.2.184:5000/webapps-devicehome:latest
-docker image rm 192.168.2.100:5000/webapps-devicehome:latest
